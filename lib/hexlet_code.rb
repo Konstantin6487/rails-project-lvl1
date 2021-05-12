e2728a4fb8e9bb489908842a8ada9b2431806999
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-05-12 23:12:07 +0300

    add single tag builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,7 +2,36 @@
 
 require_relative 'hexlet_code/version'
 
+def parse(raw)
+  tag_name, attrs = raw
+  {
+    tag_name: tag_name,
+    attrs: attrs || {},
+    body: block_given? ? yield : ''
+  }
+end
+
+def render(parsed)
+  single_tags_list = %w[area base br col embed hr img input link meta param source track wbr]
+  tag_name, attrs, body = parsed.values_at(:tag_name, :attrs, :body)
+  build_attrs = lambda { |attrs_hash|
+    attrs_hash.keys.map { |attr| " #{attr}=\"#{attrs_hash[attr]}\"" }.join
+  }
+  if single_tags_list.include?(tag_name)
+    "<#{tag_name}#{build_attrs.call(attrs)}>"
+  else
+    "<#{tag_name}#{build_attrs.call(attrs)}>#{body}</#{tag_name}>"
+  end
+end
+
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Tag builder
+  class Tag
+    def self.build(*tag, &block)
+      parsed = parse(tag, &block)
+      render parsed
+    end
+  end
 end
