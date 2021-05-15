cc8e3b8f2837c9cb86fe1d541b5d7650d350b7fe
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-05-15 20:18:04 +0300

    refactoring

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -11,12 +11,12 @@ class HexletCodeTest < Minitest::Test
     @builder = HexletCode::Tag
   end
 
-  def single_tag_build
+  def test_single_tag_build
     assert_equal('<br>', @builder.build('br'))
     assert_equal('<input type="submit" value="Save">', @builder.build('input', type: 'submit', value: 'Save'))
   end
 
-  def paired_tag_build
+  def test_paired_tag_build
     assert_equal('<label>Email</label>', @builder.build('label') { 'Email' })
     assert_equal('<label for="email">Email</label>', @builder.build('label', for: 'email') { 'Email' })
   end
