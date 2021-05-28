ad3b45cb9d9f7d1da3eee3f196600efc019f8bd3
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-05-28 18:17:30 +0300

    move files

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,28 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
-require_relative './render'
-require_relative './parser'
+
+def parse(raw)
+  tag_name, attrs = raw
+  {
+    tag_name: tag_name,
+    attrs: attrs || {},
+    body: block_given? ? yield : ''
+  }
+end
+
+def render(parsed)
+  single_tags_list = %w[area base br col embed hr img input link meta param source track wbr]
+  tag_name, attrs, body = parsed.values_at(:tag_name, :attrs, :body)
+  build_attrs = lambda { |attrs_hash|
+    attrs_hash.keys.map { |attr| " #{attr}=\"#{attrs_hash[attr]}\"" }.join
+  }
+  if single_tags_list.include?(tag_name)
+    "<#{tag_name}#{build_attrs.call(attrs)}>"
+  else
+    "<#{tag_name}#{build_attrs.call(attrs)}>#{body}</#{tag_name}>"
+  end
+end
 
 # main Module
 module HexletCode
