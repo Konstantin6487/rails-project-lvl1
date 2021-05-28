f8b0eb8def0b613cb66f08f495c6f44eb377d1f0
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-05-28 18:01:33 +0300

    refactoring tests

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -7,23 +7,52 @@ class HexletCodeTest < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_withoud_tags
+  def test_empty_tags
     user = Struct.new(:name, :job, keyword_init: true)
     init_user = user.new name: 'rob', job: 'hexlet'
-    actual = HexletCode.form_for init_user do |f|
-    end
+    actual = HexletCode.form_for init_user
     expected = '<form action="#" method="post"></form>'
     assert_equal(expected, actual)
   end
 
-  def test_added_tags
+  def test_tags
     user = Struct.new(:name, :job, keyword_init: true)
     init_user = user.new name: 'rob', job: 'hexlet'
     actual = HexletCode.form_for init_user do |f|
       f.input :name
       f.input :job, as: :textarea
     end
-    expected = '<form action="#" method="post"><input name="rob"><textarea job="hexlet"></textarea></form>'
+    expected = '<form action="#" method="post">'\
+    '<input name="rob"><textarea job="hexlet"></textarea>'\
+    '</form>'
+    assert_equal(expected, actual)
+  end
+
+  def test_tags_with_submit
+    user = Struct.new(:name, :job, keyword_init: true)
+    init_user = user.new name: 'rob', job: 'hexlet'
+    actual = HexletCode.form_for init_user do |f|
+      f.input :name
+      f.input :job
+      f.submit
+    end
+    expected = '<form action="#" method="post"><input name="rob">'\
+    '<input job="hexlet"><input type="submit" value="Save">'\
+    '</form>'
+    assert_equal(expected, actual)
+  end
+
+  def test_tags_with_submit_label
+    user = Struct.new(:name, :job, keyword_init: true)
+    init_user = user.new name: 'rob', job: 'hexlet'
+    actual = HexletCode.form_for init_user do |f|
+      f.input :name
+      f.input :job
+      f.submit 'Some'
+    end
+    expected = '<form action="#" method="post"><input name="rob">'\
+    '<input job="hexlet"><input type="submit" value="Some">'\
+    '</form>'
     assert_equal(expected, actual)
   end
 end
