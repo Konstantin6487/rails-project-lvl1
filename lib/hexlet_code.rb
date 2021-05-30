3589f3f39a6d5cb827fe156c7d75988361c3c777
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-05-30 17:51:36 +0300

    refactoring attrs

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,21 +6,21 @@ require_relative './render'
 
 # main Module
 module HexletCode
-  @stringified_tags = []
+  @tags_data = []
   class Error < StandardError; end
 
   def self.add_methods(hash) # rubocop:disable Metrics/AbcSize
-    hash.define_singleton_method(:stringified_tags) do
-      HexletCode.instance_variable_get(:@stringified_tags)
+    hash.define_singleton_method(:tags_data) do
+      HexletCode.instance_variable_get(:@tags_data)
     end
 
-    hash.define_singleton_method(:stringified_tags_push) do |value|
-      tags = HexletCode.instance_variable_get(:@stringified_tags)
+    hash.define_singleton_method(:tags_data_push) do |value|
+      tags = HexletCode.instance_variable_get(:@tags_data)
       tags.push(value)
     end
 
-    hash.define_singleton_method(:stringified_tags_clear) do
-      HexletCode.instance_variable_set(:@stringified_tags, [])
+    hash.define_singleton_method(:tags_data_clear) do
+      HexletCode.instance_variable_set(:@tags_data, [])
     end
 
     hash.define_singleton_method(:input) do |*attrs, **options|
@@ -34,23 +34,28 @@ module HexletCode
                     .select { |attr| attrs.include? attr }
                     .merge(options)
                     .reject { |key| key.eql?(:as) }
-      value = HexletCode::Tag.build(tag_name, tag_options)
-      stringified_tags_push(value)
+      # label = { tag_name: 'label', tag_options: { for: tag_options.fetch(:name, '') } }
+      input = { tag_name: tag_name, tag_options: tag_options }
+      # tags_data_push(label)
+      tags_data_push(input)
     end
 
     hash.define_singleton_method(:submit) do |label = 'Save'|
-      tag_type = 'input'
+      tag_name = 'input'
       tag_options = { type: 'submit', value: label }
-      value = HexletCode::Tag.build(tag_type, tag_options)
-      stringified_tags_push(value)
+      submit = { tag_name: tag_name, tag_options: tag_options }
+      tags_data_push(submit)
     end
   end
 
   def self.form_for(user, url: '#')
     HexletCode.add_methods(user)
     block_given? && (yield user)
-    generated_form = "<form action=\"#{url}\" method=\"post\">#{user.stringified_tags.join}</form>"
-    user.stringified_tags_clear
+    stringified_fields = user.tags_data
+                             .map { |tag| HexletCode::Tag.build(tag[:tag_name], tag[:tag_options]) }
+                             .join
+    generated_form = "<form action=\"#{url}\" method=\"post\">#{stringified_fields}</form>"
+    user.tags_data_clear
     generated_form
   end
 
