adba0eb6e29aba6f2f7d9466e6d107b21fe5c2b9
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-06-09 01:21:05 +0300

    rename files, refactoring

--- lib/hexlet_code/form_renderer.rb
+++ lib/hexlet_code/form_renderer.rb
@@ -7,7 +7,8 @@ module HexletCode
 
     def initialize(options)
       default_options = {
-        action: options.fetch(:url, '#')
+        action: options.fetch(:url, '#'),
+        method: 'post'
       }
       collected_options = default_options.merge options.except(:url)
       @options = collected_options
@@ -18,7 +19,7 @@ module HexletCode
       stringified_inner_tags = state.map do |tag_data|
         Tag.build tag_data[:tag_name], **tag_data[:tag_options], &tag_data[:tag_body]
       end.join
-      "<form#{stringified_options} method=\"post\">#{stringified_inner_tags}</form>"
+      "<form#{stringified_options}>#{stringified_inner_tags}</form>"
     end
   end
 end
