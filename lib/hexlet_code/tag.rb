10fabb7909b02f2ab831c455b99b1348bd010de9
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-08-06 21:17:05 +0300

    change field_renderer to tag

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,32 +1,28 @@
 # frozen_string_literal: true
 
 module HexletCode
-  # Field render
-  class FieldRenderer
+  # Tag render
+  class Tag
     SINGLE_TAGS_LIST = %w[area base br col embed hr img input link meta param source track wbr].freeze
 
     def build_attrs(attrs_data)
       return '' if attrs_data.empty?
 
-      attrs_data.keys.map { |attr| " #{attr}=\"#{attrs_data[attr]}\"" }.join
-    end
-
-    def render_tag_base(tag_data)
-      tag_name = tag_data[:tag_name]
-      stringified_attrs = build_attrs(tag_data[:tag_options] || {})
-      stringified_label = render(tag_data[:tag_label] || {})
-      "#{stringified_label}<#{tag_name}#{stringified_attrs}>"
+      attrs_data.map { |key, value| " #{key}=\"#{value}\"" }.join
     end
 
     def render_single_tag(tag_data)
-      render_tag_base tag_data
+      tag_name = tag_data[:tag_name]
+      stringified_attrs = build_attrs(tag_data[:tag_options] || {})
+      "<#{tag_name}#{stringified_attrs}>"
     end
 
     def render_paired_tag(tag_data)
       tag_name = tag_data[:tag_name]
       tag_body = tag_data[:tag_body] || ''
+      stringified_attrs = build_attrs(tag_data[:tag_options] || {})
       stringified_body = tag_name.eql?('label') ? tag_body.capitalize : tag_body
-      "#{render_tag_base tag_data}#{stringified_body}</#{tag_name}>"
+      "<#{tag_name}#{stringified_attrs}>#{stringified_body}</#{tag_name}>"
     end
 
     def render(tag_data)
@@ -39,5 +35,7 @@ module HexletCode
         render_paired_tag tag_data
       end
     end
+
+    private :build_attrs, :render_single_tag, :render_paired_tag
   end
 end
